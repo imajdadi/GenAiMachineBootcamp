ab0f94e5cab384e80d28bf228764e854d56cf043
imajdadi <imanemajda2@gmail.com>
2025-02-10 14:57:28 +0100

    commit

---
+++ GenAiMachineBootcamp/Prepa/CrashCs/main.py
@@ -0,0 +1,8 @@
+import random
+random_number = random.randint(1, 100)
+max_attempts = 7
+count = 0  # Initialisation du compteur
+print("hello word")
+print("The answer is", 42)  # String and integer
+print(3.14, "is a float")  # Float and string
+print(True)  # Boolean (True/False)
