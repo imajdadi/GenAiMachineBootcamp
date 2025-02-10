fbb00115572b6df2bed4bfa72be86f553c31eff8
imajdadi <imanemajda2@gmail.com>
2025-02-10 15:03:22 +0100

    new dev

--- GenAiMachineBootcamp/Prepa/CrashCs/main.py
+++ GenAiMachineBootcamp/Prepa/CrashCs/main.py
@@ -6,3 +6,13 @@ print("hello word")
 print("The answer is", 42)  # String and integer
 print(3.14, "is a float")  # Float and string
 print(True)  # Boolean (True/False)
+if num_user < random_number:
+        print("Too low!")
+    elif num_user > random_number:
+        print("Too high!")
+    elif num_user == random_number:
+        print("félicitations ")
+        break
+
+    if attempt_Game == 6 :
+        print(f"révélant le numéro {random_number} correct.")
